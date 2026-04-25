351b494f26dcb49946dbf2c2d33df88e899a4cce
11249a212-png <11249a212@kanchiuniv.ac.in>
2026-04-25 18:28:16 +0530

    towerofhanoi.c

--- DAA/towerofhanoi.c
+++ DAA/towerofhanoi.c
@@ -1,21 +1,35 @@
 #include<stdio.h>
+
+// Function for Tower of Hanoi
 void TOH(int n, char s, char t, char d)
 {
-   // printf("hi, this is tower of hanoi");
+    // Base condition: if only 1 disk, move directly
     if(n==1)
     {
-        printf("move %d from %c to %c :\n",n,s,d);
+        printf("move %d from %c to %c :\n", n, s, d);
         return;
     }
-    TOH(n-1,s,d,t);
-    printf("move %d from %c to %c:\n",n,s,d);
-    TOH(n-1,t,s,d);
+
+    // Step 1: move n-1 disks from source to temporary using destination
+    TOH(n-1, s, d, t);
+
+    // Step 2: move nth (largest) disk from source to destination
+    printf("move %d from %c to %c:\n", n, s, d);
+
+    // Step 3: move n-1 disks from temporary to destination using source
+    TOH(n-1, t, s, d);
 }
+
 int main()
 {
-int n ;
-printf("enter number of disks:");
-scanf("%d",&n);
-TOH(n,'s','t','d');
+    int n;
 
+    // Taking input from user
+    printf("enter number of disks:");
+    scanf("%d", &n);
+
+    // Calling function with s=source, t=temporary, d=destination
+    TOH(n, 's', 't', 'd');
+
+    return 0;
 }
